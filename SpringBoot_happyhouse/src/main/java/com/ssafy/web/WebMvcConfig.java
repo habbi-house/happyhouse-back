498b7c59725a4126fbc3f3aba53b0f31b0ab95e2
kancho1216 <kancho1216@naver.com>
2022-05-06 13:18:18 +0900

    ✨ Impl LoginInterceptor + Add Interceptor

---
+++ SpringBoot_happyhouse/src/main/java/com/ssafy/web/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.ssafy.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ssafy.web.interceptor.LoginInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		// 로그인 되어 있는 사용자만 허용
+		registry.addInterceptor(new LoginInterceptor())
+				.addPathPatterns("/user/**")
+				.addPathPatterns("/board/create")
+				.addPathPatterns("/board/**/update")
+				.addPathPatterns("/board/**/delete")
+				.excludePathPatterns("/user/login")
+				.excludePathPatterns("/user/signIn");
+	}
+}
---
+++ SpringBoot_happyhouse/src/main/java/com/ssafy/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.ssafy.web.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.ssafy.web.vo.UserVO;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession(false);
+		
+		if(session != null) {
+			UserVO user = (UserVO) session.getAttribute("user");
+			if(user != null) {
+				return true;
+			}
+		}
+		
+		response.sendRedirect("/login");
+		return false;
+	}
+
+}
